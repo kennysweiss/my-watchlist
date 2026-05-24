b1cb8bfc974624ea9f4922ef2b8d7bf4bded3bef
kennysweiss <111298681+kennysweiss@users.noreply.github.com>
2026-05-24 14:08:31 -0400

    Add API handler for Supabase configuration

---
+++ api/config.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    supabaseUrl: 'https://szkyjhgxvplemaeqnqwc.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6a3lqaGd4dnBsZW1hZXFucXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMTY3MjksImV4cCI6MjA4OTY5MjcyOX0.3wJc1fPfH2wzgnB4_efp7j0jMPAjbw7NsyZHrJf0kXU'
+  });
+}
